5f1a82ae78914af63091603c94decc4209c51e32
mihir <mihir@gmail.com>
2024-03-06 17:34:47 +0530

    main file is added

---
+++ public_html/register.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CAMERA INFORMATION MANAGEMENT SYSTEM</title>
+
+    <!-- font font-awesome cdn -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!-- scripts jquery -->
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    
+    <!-- scripts bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q     +zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+    <!-- bootstrap cdn -->
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <!-- custom script -->
+    <link rel="stylesheet" href="css/register.css">
+
+</head>
+<body>
+    <div class="container-fluid g-0">
+    
+
+
+
+    <div class="con">
+    <div class="form">
+        <h1>Register</h1>
+        <form id="login">
+
+                <br>
+                <br>
+        <div class="wrapper">
+                <div class="textInputWrapper">
+                <input placeholder="UserName" type="text" class="textInput">
+                </div>
+                <div class="textInputWrapper">
+                <input placeholder="UserName" type="text" class="textInput">
+                </div>
+        </div>
+                <br>
+
+        <div class="wrapper">
+                <div class="textInputWrapper">
+                <input placeholder="Password" type="Password" class="textInput">
+                </div>
+
+                <div class="textInputWrapper">
+                <input placeholder="Password" type="Password" class="textInput">
+                </div>
+        </div>
+
+                <br>
+                <div class="button">
+                <input type="submit" value="submit" name="submit" id="submit">
+                </div>
+                <div class="go-r-f">
+                <a href="#">Registration!</a>
+                </div>
+                <div class="go-r-f">
+                <a href="#">forget Password?</a>
+                </div>
+
+        </form>
+    </div>
+    </div>
+    </div>
+
+</body>
+<script src="js/main.js" ></script>
+</html>
